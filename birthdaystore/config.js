87428f29a83acf5a21dc71f9cbe9885a0baaeaf5
emad313 <emad.uddin.0001@gmail.com>
2024-02-10 13:19:30 +0600

    birthday

---
+++ birthdaystore/config.js
@@ -0,0 +1,31 @@
+// Description: Configuration file for birthdaystore.
+var config = {
+    texts: [
+        "Happy birthday, Mastura, my heart's dearest delight,",
+        "In your presence, life's colors ignite.",
+        "With each smile, you light up my world,",
+        "Your love, a banner gloriously unfurled.",
+        "In your embrace, I find solace and peace,",
+        "Your laughter, a melody that will never cease.",
+        "With you, my love, I am complete,",
+        "In your arms, I find my retreat.",
+        "Today, I celebrate your beauty and grace,",
+        "May joy and love in your life find their place.",
+        "Happy birthday, my beloved Wife, forever my guiding light,",
+        "With you, every moment is pure delight."
+
+    ],
+    imgs: {
+        "Lovelycutie": ".birthdaystore/imgs/hand.jpg",
+    },
+    desc: {
+        turn_on: "Let's start",
+        play: "Music please",
+        bannar_coming: "Colorful banners?",
+        balloons_flying: "Here comes the balloons",
+        cake_fadein: "Delicious cake?",
+        light_candle: "Light the candles",
+        wish_message: "Happy Birthday, wifey",
+        story: "A SIMPLE MESSAGE FOR YOU",
+    }
+};
